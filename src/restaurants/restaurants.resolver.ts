f67f101cd387bce8bdda235226af0b98e3fd44d6
Kim HyeonSang <k98hh@naver.com>
2023-05-12 01:00:40 +0900

    resolver 까지

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  @Query((returns) => Restaurant)
+  myRestaurant() {
+    return true;
+  }
+}
